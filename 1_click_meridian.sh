743f6f2916d31284cc1090aa249f59029118edc1
Michael Pedersen <mpeder@google.com>
2025-04-01 20:10:35 +0200

    wqeqwe

--- 1_click_meridian.sh
+++ 1_click_meridian.sh
@@ -4,49 +4,25 @@ set -e
 
 echo "Deploying Meridian"
 
-: "${LOCATION:=us-central1}" && export LOCATION
-export PROJECT_ID=$(gcloud config get-value project)
+# : "${LOCATION:=us-central1}" && export LOCATION
+# export PROJECT_ID=$(gcloud config get-value project)
 
-gcloud services enable bigquery.googleapis.com \
-                       cloudbuild.googleapis.com \
-                       composer.googleapis.com \
-                       storage-component.googleapis.com \
-                       cloudresourcemanager.googleapis.com \
-                       dataflow.googleapis.com \
-                       datacatalog.googleapis.com
+# gcloud services enable bigquery.googleapis.com \
+#                        cloudbuild.googleapis.com \
+#                        composer.googleapis.com \
+#                        storage-component.googleapis.com \
+#                        cloudresourcemanager.googleapis.com \
+#                        dataflow.googleapis.com \
+#                        datacatalog.googleapis.com
+
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_K9_REPORTING
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GADS_REPORTING
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_TIKTOK_REPORTING
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_META_REPORTING
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_DV360_REPORTING
+# bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_ORACLE_EBS_REPORTING
 
 
-
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_K9_PROCESSING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_K9_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_VERTEX_DATASET
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SAP_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SAP_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SAP_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SAP_ML_MODELS
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFDC_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFDC_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFDC_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_CM360_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_CM360_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_CM360_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GADS_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GADS_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GADS_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_TIKTOK_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_TIKTOK_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_TIKTOK_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_LIVERAMP_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_META_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_META_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_META_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFMC_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFMC_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_SFMC_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_DV360_RAW
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_DV360_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_DV360_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GA4_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_GA4_REPORTING
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_ORACLE_EBS_CDC
-bq --location=${LOCATION} mk -d ${PROJECT_ID}:CORTEX_ORACLE_EBS_REPORTING
+# export PROJECT_ID=$(gcloud config get-value project)
+# gcloud builds get-default-service-account --project ${PROJECT_ID}
+# export CLOUD_BUILD_SA=<INSERT CLOUD BUILD DEFAULT SERVICE ACCOUNT HERE>
--- deploy_guide.md
+++ deploy_guide.md
@@ -4,9 +4,22 @@
 ## What is Google Cloud Cortex Framework for Meridian?
 
 TODO
-The Data Foundation for [Google Cloud Cortex Framework](https://cloud.google.com/solutions/cortex) is a set of analytical artifacts, that can be automatically deployed together with reference architectures.
+Cortex Framework for Meridian is bla bla bla
 
-This tutorial will guide you through Data Foundation configuration and deployment.
+For more information, see:
+
+- [Google Cloud Cortex Framework](https://cloud.google.com/cortex/docs/overview) 
+- [Cortex Framework for Marketing](https://cloud.google.com/cortex/docs/data-sources-and-workloads#marketing)
+- [Meridian](https://developers.google.com/meridian)
+
+This tutorial will guide you through demo deployment of Cortex Framework for Meridian in your own Google Cloud project.
+
+This deployment includes:
+- TODO
+- TODO
+- Oracle EBS sales data
+
+For a full production deployment please follow the [full deployment steps](https://cloud.google.com/cortex/docs/overview#deployment)
 
 Click the **Start** button to move to the next step.
 
@@ -17,28 +30,38 @@ Click the **Start** button to move to the next step.
 
 Cortex for Meridian will be installed in the selected project: `<walkthrough-project-id/>`
 
-The default region used is: `us-central1`
+The default region used is: `us-central1`. Make sure you select this when viewing e.g. Colab Enterprise runtime templates and executions.
 
 Click the **Next** button to move to the next step.
 
 ## Enable APIs
 <walkthrough-cloud-shell-icon></walkthrough-cloud-shell-icon>
 
+Before starting deployment we need to enable the following APIs:
+
 <walkthrough-enable-apis apis="bigquery.googleapis.com,cloudbuild.googleapis.com"></walkthrough-enable-apis>
 
-## Open Cloud Shell
-<walkthrough-open-cloud-shell-button></walkthrough-open-cloud-shell-button>
+After enabling the APIs, click the **Next** button to move to the next step.
 
 ## Create Meridian Runner Service Account
 <walkthrough-cloud-shell-icon></walkthrough-cloud-shell-icon>
 
+The Workflow and Colab Enterprise notebook use for running Meridian with Cortex marketing and sales data requires a seperate Service Account.
 
+This service account will be created automatically when you begin
 
+To create the Service Account run this command:
 
 ```sh
 gcloud iam service-accounts create cortex-meridian-colab-runner \
   --description="Cortex for Meridian Colab Runner Service Account" \
   --display-name="Cortex Meridian Runner"
+```
+
+Assign the required roles to the Service Account with this command:
+
+```sh
+gcloud projects add-iam-policy-binding
 ```
 
 ## Start deployment
@@ -55,10 +78,14 @@ The deployment is now running please wait for the Cloud Build deployment to comp
 
 [Go to Cloud Build](https://console.cloud.google.com/cloud-build/builds).
 
-[Go to Cloud Build](https://console.cloud.google.com/cloud-build/builds?target=_blank).
+[Go to Workflows](https://console.cloud.google.com/workflows/workflow/us-central1/cortex-meridian-execute-notebook/executions)
+
+[Go to Notebook Executions](https://console.cloud.google.com/vertex-ai/colab/execution-jobs)
+
+Click the **Next** button to finish.
 
 ## Conclusion
 
-Thanks for using Google Cloud Cortex Framework for Meridian!
+Thanks for trying Google Cloud Cortex Framework for Meridian!
 
 <walkthrough-conclusion-trophy></walkthrough-conclusion-trophy>
