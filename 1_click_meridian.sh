f01bcb15738cf87c2bd1b3f2246829b51bee98ff
Michael Pedersen <mpeder@google.com>
2025-04-02 15:30:58 +0200

    ewewrw

--- 1_click_meridian.sh
+++ 1_click_meridian.sh
@@ -19,9 +19,9 @@ done
 if [ -n "$PROJECT_IN" ]; then
   echo "Deploying Meridian"
 
-    echo "Setting active project to: ${1}"
+    echo "Setting active project to: ${PROJECT_IN}"
 
-    gcloud config set project "${1}"
+    gcloud config set project "${PROJECT_IN}"
 
 export PROJECT_ID=$(gcloud config get-value project)
 else
